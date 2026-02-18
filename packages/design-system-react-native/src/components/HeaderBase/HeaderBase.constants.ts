05886fb0ff7b0c234324c6434eb2e119b24da805
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-18 19:23:01 +0100

    feat: remove `variant` prop

--- packages/design-system-react-native/src/components/HeaderBase/HeaderBase.constants.ts
+++ packages/design-system-react-native/src/components/HeaderBase/HeaderBase.constants.ts
@@ -1,20 +1,3 @@
-// External dependencies.
-import { TextVariant } from '../Text';
-
-// Internal dependencies.
-import { HeaderBaseVariant } from './HeaderBase.types';
-
-/**
- * Text variant mapping based on HeaderBase variant.
- */
-export const HEADERBASE_VARIANT_TEXT_VARIANTS: Record<
-  HeaderBaseVariant,
-  TextVariant
-> = {
-  [HeaderBaseVariant.Compact]: TextVariant.HeadingSm,
-  [HeaderBaseVariant.Display]: TextVariant.HeadingLg,
-};
-
 /**
  * Default test ID for the title Text element.
  */
